70f179a6f6e26b63f25888984245b84549227238
amartin755 <netnag@mailbox.org>
2023-10-03 12:33:03 +0200

    Updated german translations

--- src/I10n/stechuhr_de.ts
+++ src/I10n/stechuhr_de.ts
@@ -10,7 +10,22 @@
         <translation>Stechuhr</translation>
     </message>
     <message>
-        <location line="+33"/>
+        <location line="+23"/>
+        <source>Undo</source>
+        <translation>Rückgängig</translation>
+    </message>
+    <message>
+        <location line="+55"/>
+        <source>Settings</source>
+        <translation>Einstellungen</translation>
+    </message>
+    <message>
+        <location line="+42"/>
+        <source>About Stechuhr</source>
+        <translation>Über Stechuhr</translation>
+    </message>
+    <message>
+        <location line="+43"/>
         <location/>
         <source>Clock in</source>
         <translation>Kommen</translation>
@@ -29,7 +44,7 @@
         <translation>Pause</translation>
     </message>
     <message>
-        <location filename="../maindialog.cpp" line="+26"/>
+        <location filename="../maindialog.cpp" line="+27"/>
         <source>Clock in</source>
         <translation>Kommen</translation>
     </message>
@@ -49,7 +64,7 @@
         <translation>Pause beenden</translation>
     </message>
     <message>
-        <location line="+27"/>
+        <location line="+39"/>
         <source>Application has been closed while clocked in.</source>
         <translation>Die Anwendung wurde beendet ohne &apos;Gehen&apos; zu buchen.</translation>
     </message>
@@ -59,7 +74,7 @@
         <translation>Fortsetzen</translation>
     </message>
     <message>
-        <location line="+10"/>
+        <location line="+5"/>
         <source>Do you want to continue, clock out or discard?</source>
         <translation>Möchten Sie die gespeicherte Sitzung fortsetzen, &apos;Gehen&apos; buchen oder verwerfen?</translation>
     </message>
@@ -67,6 +82,16 @@
         <location line="+6"/>
         <source>Do you want to continue or discard?</source>
         <translation>Möchten Sie die gespeicherte Sitzung fortsetzen oder verwerfen?</translation>
+    </message>
+    <message>
+        <location line="+169"/>
+        <source>About Stechuhr</source>
+        <translation>Über Stechuhr</translation>
+    </message>
+    <message>
+        <location line="+0"/>
+        <source>&lt;H2&gt;&lt;b&gt;Stechuhr&lt;/b&gt;&lt;/H2&gt;Version %1&lt;br&gt;&lt;br&gt;A working time tracking utility.&lt;br&gt;&lt;a href=https://github.com/amartin755/stechuhr&gt;https://github.com/amartin755/stechuhr&lt;/a&gt;&lt;br&gt;&lt;br&gt;Copyright (C) 2023 Andreas Martin&lt;br&gt;&lt;br&gt;This program is covered by the GNU General Public License, version 3 (GPLv3),&lt;a href=http://www.gnu.org/licenses&gt;http://www.gnu.org/licenses&lt;/a&gt;&lt;br&gt;It uses the 3rd party components, covered by their respective license:&lt;br&gt;&lt;br&gt;QT [v%2] (&lt;a href=http://qt.io&gt;http://qt.io&lt;/a&gt;)&lt;br&gt;Breezy icons (&lt;a href=https://github.com/KDE/breeze-icons&gt;https://github.com/KDE/breeze-icons&gt;&lt;/a&gt;)</source>
+        <translation>&lt;H2&gt;&lt;b&gt;Stechuhr&lt;/b&gt;&lt;/H2&gt;Version %1&lt;br&gt;&lt;br&gt;Ein einfaches Programm zur Erfassung der Arbeitszeit.&lt;br&gt;&lt;a href=https://github.com/amartin755/stechuhr&gt;https://github.com/amartin755/stechuhr&lt;/a&gt;&lt;br&gt;&lt;br&gt;Copyright (C) 2023 Andreas Martin&lt;br&gt;&lt;br&gt;Dieses Programm ist freie Software. Sie können es weitergeben oder ändern, solange Sie dabei den Bedingungen der GNU General Public License Version 3 (GPL), wie von der Free Software Foundation herausgegeben, folgen, &lt;a href=http://www.gnu.org/licenses&gt;http://www.gnu.org/licenses&lt;/a&gt;&lt;br&gt;Ebenso werden Komponenten von Drittanbietern verwendet, die jeweils unter ihrer eigenen Lizenz stehen:&lt;br&gt;&lt;br&gt;QT [v%2] (&lt;a href=http://qt.io&gt;http://qt.io&lt;/a&gt;)&lt;br&gt;Breezy icons (&lt;a href=https://github.com/KDE/breeze-icons&gt;https://github.com/KDE/breeze-icons&gt;&lt;/a&gt;)</translation>
     </message>
 </context>
 </TS>
