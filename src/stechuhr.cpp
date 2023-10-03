edfb4f549278fb6c9281af4dff4adf08d7091aaf
amartin755 <netnag@mailbox.org>
2023-10-03 10:30:25 +0200

    fix frozen working time fixes #3

--- src/stechuhr.cpp
+++ src/stechuhr.cpp
@@ -105,7 +105,7 @@ void Stechuhr::getWorkingTime (unsigned &hours, unsigned &minutes) const
     bool clockedIn = hasClockedIn ();
     bool round = !clockedIn; // round when work is finished
 
-    QDateTime currTime = takesBreak () || clockedIn ? m_events.last().second : QDateTime::currentDateTime();
+    QDateTime currTime = takesBreak () || !clockedIn ? m_events.last().second : QDateTime::currentDateTime();
     qint64 elapsedSecs = m_events.first().second.secsTo (currTime) - getBreakDuration ();
 
     Q_ASSERT (elapsedSecs >= 0);
