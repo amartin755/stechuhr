22bad306bc144dbe01fd0c2c292f4a4b5bf9cff3
Andreas Martin <netnag@mailbox.org>
2024-04-01 21:34:12 +0200

    removed usage of string literals _L1
    They are not really needed here and need  qt >= 6.4

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,6 @@
 
 #include "maindialog.h"
 
-using namespace Qt::StringLiterals;
 
 int main (int argc, char** argv)
 {
@@ -31,7 +30,7 @@ int main (int argc, char** argv)
 
     auto locale = QLocale::system();
     QTranslator translator;
-    if (translator.load(QLocale(), "stechuhr"_L1, "_"_L1, ":/i18n"_L1))
+    if (translator.load(QLocale(), "stechuhr", "_", ":/i18n"))
         app.installTranslator(&translator);   
 
     QCoreApplication::setApplicationName ("Stechuhr");
